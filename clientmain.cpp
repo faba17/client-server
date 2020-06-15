314200418ae53a387e3459840bcdabbd2682dd88
faba17 <43542742+faba17@users.noreply.github.com>
2020-06-15 23:42:20 +0200

    Update clientmain.cpp

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 // Included to get the support library
 #include <calcLib.h>
+
+#define MAX_LINE_SIZE 256
 
 int main(int argc, char *argv[]){
   
