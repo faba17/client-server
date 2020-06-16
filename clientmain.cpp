5bbdc92ac3b1e84b04d190d2edbe62ca096cc37e
faba17 <43542742+faba17@users.noreply.github.com>
2020-06-16 02:12:18 +0200

    Update clientmain.cpp

--- clientmain.cpp
+++ clientmain.cpp
@@ -4,11 +4,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include <string.h>
 // Included to get the support library
 #include <calcLib.h>
 
 #define MAX_LINE_SIZE 256
+
+void read_line(int sockd, char *line);
 
 int main(int argc, char *argv[]){
   
