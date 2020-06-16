529387732b8db45f945a61545c026d9c07859f54
faba17 <43542742+faba17@users.noreply.github.com>
2020-06-16 02:22:34 +0200

    Update servermain.cpp

--- servermain.cpp
+++ servermain.cpp
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 // Included to get the support library
 #include <calcLib.h>
-
-
+#define MAX_LINE_SIZE 256
 
 using namespace std;
-
+void read_line(int sockd, char *line);
 
 int main(int argc, char *argv[]){
   
