d6e1b72c4be0b28d1c683f713864007f4341ac53
faba17 <43542742+faba17@users.noreply.github.com>
2020-06-18 00:10:13 +0200

    Update servermain.cpp

--- servermain.cpp
+++ servermain.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /* You will to add includes here */
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,18 +8,16 @@
 #include <unistd.h>
 #include <string.h>
 
-// Included to get the support library
 #include <calcLib.h>
+
 #define MAX_LINE_SIZE 256
 
-using namespace std;
 void read_line(int sockd, char *line);
 void createEquation(char* buffer, char* result);
 
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
-  char ip[50];
+int main(int argc, char *argv[]) {
+
+	char ip[50];
 	int port = 0;
 	char buffer[MAX_LINE_SIZE];
 
@@ -38,26 +37,29 @@ int main(int argc, char *argv[]){
 		printf("Usage2: ./server ipto_listenon:port\n");
 		return 0;
 	}
-  
-  struct sockaddr_in addr;
+
+	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
 	addr.sin_addr.s_addr = inet_addr(ip);
 	memset(&(addr.sin_zero), 0, 8);
-	
-  int serverSockd = socket(AF_INET, SOCK_STREAM, 0);
-  socklen_t addr_len = sizeof(struct sockaddr_in);
-  if (bind(serverSockd, (struct sockaddr *) &addr, addr_len) == -1) {
-      printf("bind error\n");
-      return 0;
-   }
-	
-   if (listen(serverSockd, 10) == -1) {
+
+	int serverSockd = socket(AF_INET, SOCK_STREAM, 0);
+
+	socklen_t addr_len = sizeof(struct sockaddr_in);
+	if (bind(serverSockd, (struct sockaddr *) &addr, addr_len) == -1) {
+		printf("bind error\n");
+		return 0;
+	}
+
+
+	if (listen(serverSockd, 10) == -1) {
 		printf("listen error\n");
 		return 0;
-   }
-	
-initCalcLib();
+	}
+
+
+	initCalcLib();
 
 	while (1) {
 		
@@ -71,9 +73,11 @@ initCalcLib();
 			continue;
 		}
 
+	
 		sprintf(buffer, "TEXT TCP 1.0\n\n");
 		send(sockd, buffer, strlen(buffer), 0);
 
+	
 		read_line(sockd, buffer);
 		if (strcmp("OK", buffer) == 0) {
 			char result[100];
@@ -81,8 +85,9 @@ initCalcLib();
 			createEquation(buffer, result);
 
 			printf("%sanswer:%s\n", buffer, result);
-			
-                send(sockd, buffer, strlen(buffer), 0);
+
+
+			send(sockd, buffer, strlen(buffer), 0);
 
 			read_line(sockd, userResult);
 			printf("user: %s\n", userResult);
@@ -120,18 +125,22 @@ void read_line(int sockd, char *line) {
 }
 
 void createEquation(char* buffer, char* result) {
-	char *ptr = randomType(); // Get a random arithemtic operator.
+	char *ptr = randomType(); 
 
 	double f1, f2, fresult;
 	int i1, i2, iresult;
 
-	
+	/*
+	 printf("ptr = %p, \t", ptr );
+	 printf("string = %s, \n", ptr );
+	 */
+
+
 	if (ptr[0] == 'f') {
 		//printf("Float\t");
 		f1 = randomFloat();
 		f2 = randomFloat();
 
-		
 		sprintf(buffer, "%8.8g %8.8g", f1, f2);
 		sscanf(buffer, "%lf%lf", &f1, &f2);
 
@@ -146,8 +155,7 @@ void createEquation(char* buffer, char* result) {
 		}
 		sprintf(buffer, "%s %8.8g %8.8g\n", ptr, f1, f2);
 		sprintf(result, "%8.8g", fresult);
-	}
-else {
+	} else {
 		//printf("Int\t");
 		i1 = randomInt();
 		i2 = randomInt();
@@ -167,3 +175,5 @@ else {
 	}
 
 }
+
+
